21833c47d70d2415cd3cc5c2a0dc73327d2b0c53
tanmayee2 <tatajane@iu.edu>
2024-04-05 03:29:14 -0400

    rabbitmq

--- src/main/java/edu/iu/tatajane/primesservice/controller/AuthenticationController.java
+++ src/main/java/edu/iu/tatajane/primesservice/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin({"https://tanmayee2.github.io/SE_numbers-app/"})
 public class AuthenticationController {
 
     private final IAuthenticationService authenticationService ;
